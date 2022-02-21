9a7774c8ee8615a7809e4fa0e4f1230a7e30a71d
Eugene Dudkin <ZheDre1N@gmail.com>
2022-02-21 23:09:56 +0300

    Initial Commit

---
+++ CALayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CALayer
+//
+//  Created by Eugene Dudkin on 21.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
